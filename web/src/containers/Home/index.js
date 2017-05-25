ebd9422b1d8e6f3b7062438d95e9b16ca0a53708
Garth Gaughan <garth0323@gmail.com>
2017-05-25 15:14:35 -0700

    changes for deployment

--- web/src/containers/Home/index.js
+++ web/src/containers/Home/index.js
@@ -1,5 +1,6 @@
 // @flow
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { css, StyleSheet } from 'aphrodite';
 import { fetchRooms, createRoom, joinRoom } from '../../actions/rooms';
