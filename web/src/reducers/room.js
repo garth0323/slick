09978712841b96ea6ec45b2d621dd4831bbf9b40
Garth Gaughan <garth0323@gmail.com>
2017-05-25 12:54:31 -0700

    working with styling changes needed

--- web/src/reducers/room.js
+++ web/src/reducers/room.js
@@ -1,8 +1,16 @@
+
 const initialState = {
   channel: null,
   currentRoom: {},
   messages: [],
   presentUsers: [],
+  loadingOlderMessages: false,
+  pagination: {
+    total_pages: 0,
+    total_entries: 0,
+    page_size: 0,
+    page_number: 0,
+  },
 };
 
 export default function (state = initialState, action) {
@@ -12,7 +20,8 @@ export default function (state = initialState, action) {
         ...state,
         channel: action.channel,
         currentRoom: action.response.room,
-        messages: action.response.messages.reverse(), // new line
+        messages: action.response.messages.reverse(),
+        pagination: action.response.pagination,
       };
     case 'USER_LEFT_ROOM':
       return initialState;
@@ -28,6 +37,26 @@ export default function (state = initialState, action) {
       return {
         ...state,
         presentUsers: action.presentUsers,
+      };
+    case 'FETCH_MESSAGES_REQUEST':
+      return {
+        ...state,
+        loadingOlderMessages: true,
+      };
+    case 'FETCH_MESSAGES_SUCCESS':
+      return {
+        ...state,
+        messages: [
+          ...action.response.data.reverse(),
+          ...state.messages,
+        ],
+        pagination: action.response.pagination,
+        loadingOlderMessages: false,
+      };
+    case 'FETCH_MESSAGES_FAILURE':
+      return {
+        ...state,
+        loadingOlderMessages: false,
       };
     default:
       return state;
